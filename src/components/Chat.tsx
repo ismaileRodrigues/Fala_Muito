3dc5f66474f61c9f106d147014d185ce9b1c58ea
ismaileRodrigues <ismailearliene@gmail.com>
2026-08-22 23:20:33 -0300

    Change watermark div to absolute positioning

--- src/components/Chat.tsx
+++ src/components/Chat.tsx
@@ -977,13 +977,14 @@ export function Chat({ session }: { session: Session }) {
             {/* Mensagens com Marca d'água Neon da Bandeira Brasileira */}
             <div className="flex-1 overflow-y-auto p-4 space-y-3 bg-slate-950 relative">
               {/* Marca d'água incorporada */}
-             <div className="fixed inset-0 flex items-center justify-center pointer-events-none z-0">
-  <img 
-    src="./src/assets/logo.png" 
-    alt="Marca d'água" 
-    className="w-[380px] md:w-[500px] h-auto object-contain opacity-[0.6] filter drop-shadow-[0_0_20px_rgba(34,197,94,0.4)]"
-  />
-</div>
+              <div className="absolute inset-0 flex items-center justify-center pointer-events-none z-0">
+                <img 
+                  src="./src/assets/logo.png" 
+                  alt="Marca d'água" 
+                  className="w-[380px] md:w-[500px] h-auto object-contain opacity-[0.6] filter drop-shadow-[0_0_20px_rgba(34,197,94,0.4)]"
+                />
+              </div>
+
               {/* Listagem de Mensagens */}
               <div className="relative z-10 space-y-3">
                 {messages.map((msg) => {
